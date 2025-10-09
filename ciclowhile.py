0652e59e4f3f27aacfb46dd59e52621f2629fd02
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-10-08 22:53:53 -0600

    ciclo while

---
+++ ciclowhile.py
@@ -0,0 +1,15 @@
+# Bucle while. 
+
+import math 
+
+numero = int(input('Digite un numero: '))
+
+while numero <0:
+    print('Error -> Deberia ser un numero positivo')
+    numero = int(input('Digite un numero: '))
+
+print(f'\n Su raiz cuadrada es: {(math.sqrt(numero)): .2f}')
+
+
+
+
