71a763b71649c01d990966f05d88fc3ece6b2b48
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-10-10 01:16:20 -0600

    Ciclo while

---
+++ Ciclos/Bucle_while.py
@@ -0,0 +1,21 @@
+# Ciclo while.
+
+i = 1
+print('*** \n Para incrementar el valor de la variable**')
+while i < 5:
+    print(f'El valor del bucle es: {i}.')
+    
+    i +=1
+    
+# Decrementar el bucle. 
+print('*** \n Para Disminuir el valor de la variable**')
+while i >= -5:
+    print(f'El valor del bucle es: {i}.')
+    i -= 1
+    
+    
+    
+
+
+
+
